04716a46f51026ae1eb7e683734f4a7de8705929
Joaquín José Cruz Lorenzo <113943481+juakincruzz@users.noreply.github.com>
2025-12-17 18:23:32 +0100

    Delete p6 directory

--- p6/p6/pivote_aspas.gd
+++
@@ -1,7 +0,0 @@
-extends Node3D
-
-@export var velocidad_giro := 90.0
-
-func _physics_process(delta):
-	# Rotamos en el eje Z local (asegúrate de que las aspas giren en este eje)
-	rotate_z(deg_to_rad(velocidad_giro * delta))
